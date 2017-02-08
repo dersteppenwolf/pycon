d73d3914a97fbf9e83ee06ec648e6a9874f5ebe4
juanmendez@gkudos.com <juanmendez@gkudos.com>
2017-02-08 17:18:41 -0500

    csv to shp

--- 02_transform.py
+++ 02_transform.py
@@ -1,17 +1,21 @@
 import csv
 from shapely.geometry import Point, mapping
 from fiona import collection
+from fiona.crs import from_epsg
 
-schema = { 'geometry': 'Point', 'properties': { 'name': 'str' } }
-with collection(
-    "some.shp", "w", "ESRI Shapefile", schema) as output:
-    with open('some.csv', 'rb') as f:
+
+
+schema = { 'geometry': 'Point', 'properties': { 'depth': 'float:7.3', 'magnitude' : 'float:7.3' } }
+
+with collection("data/shp/earthquakes.shp", "w", crs=from_epsg(4326),driver='ESRI Shapefile', schema=schema) as output:
+    with open('data/earthquakes.csv', 'rb') as f:
         reader = csv.DictReader(f)
         for row in reader:
-            point = Point(float(row['lon']), float(row['lat']))
+            point = Point(float(row['Longitude']), float(row['Latitude']))
             output.write({
                 'properties': {
-                    'name': row['name']
+                    'depth': row['Depth'],
+                    'magnitude': row['Magnitude']
                 },
                 'geometry': mapping(point)
             })
